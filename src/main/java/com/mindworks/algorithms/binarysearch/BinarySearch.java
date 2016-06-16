ee81f3e067d29d32aae3f1b7ea04f2d3954e6d7c
crazy <v-muhammadfaisal@nbnco.com.au>
2016-06-16 22:19:09 +1000

    Formatted comments.

--- src/main/java/com/mindworks/algorithms/binarysearch/BinarySearch.java
+++ src/main/java/com/mindworks/algorithms/binarysearch/BinarySearch.java
@@ -1,23 +1,21 @@
 package com.mindworks.algorithms.binarysearch;
 
 /*
-Binary search is a fast search algorithm with run-time complexity of O(log n). This search algorithm works on the
-principle of divide and conquer. For this algorithm to work properly the data collection should be in sorted form.
-
-Binary search, search a particular item by comparing the middle most item of the collection.
-If match occurs then index of item is returned.
-If middle item is greater than item then item is searched in sub-array to the right of the middle item,
-other wise item is search in sub-array to the left of the middle item.
-This process continues on sub-array as well until the size of subarray reduces to zero.
-
-Class:	Search algorithm
-Data structure:	Array
-Worst case performance:	O(log n)
-Best case performance:	O(1)
-Average case performance:	O(log n)
-Worst case space complexity:	O(1) iterative,
-O(log n) recursive
-(without tail call elimination)
+ * Binary search is a fast search algorithm with run-time complexity of O(log n). This search algorithm works on the
+ * principle of divide and conquer. For this algorithm to work properly the data collection should be in sorted form.
+ *
+ * Binary search, search a particular item by comparing the middle most item of the collection.
+ * If match occurs then index of item is returned.
+ * If middle item is greater than item then item is searched in sub-array to the right of the middle item,
+ * other wise item is search in sub-array to the left of the middle item.
+ * This process continues on sub-array as well until the size of subarray reduces to zero.
+ *
+ * Class:	Search algorithm
+ * Data structure:	Array
+ * Worst case performance:	O(log n)
+ * Best case performance:	O(1)
+ * Average case performance:	O(log n)
+ * Worst case space complexity:	O(1) iterative, O(log n) recursive (without tail call elimination)
  */
 public class BinarySearch {
     private BinarySearch() {
